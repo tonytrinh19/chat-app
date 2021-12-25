5959676692cb3fe4c2268598700d8b13c9faeb03
tonytrinh19 <tony.q.trinh@gmail.com>
2021-12-24 17:04:12 -0800

    Added socket.io on client and server

--- src/index.js
+++ src/index.js
@@ -1,6 +1,13 @@
 const path = require('path')
+const http = require('http')
 const express = require('express')
+const socketio = require('socket.io')
+
+// Needs the original http because express set http behind the scene and we don't have access to http anymore
+// Do it this way to pass express to server and then create a socket server
 const app = express()
+const server = http.createServer(app)
+const io = socketio(server)
 
 
 const port = process.env.PORT
@@ -8,4 +15,11 @@ const publicDirectory = path.join(__dirname, '../public')
 
 app.use(express.static(publicDirectory))
 
-app.listen(port, () => {console.log(`App is listening on port ${port}`)})
+// On a new connection occur
+io.on('connection', () => {
+    console.log('New web socket connection.')
+})
+
+server.listen(port, () => {
+    console.log(`App is listening on port ${port}`)
+})
